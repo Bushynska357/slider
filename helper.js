0c3bf850e47c646b309a3240ffc58af65f2155d0
Bushynska357 <bushynskaja@gmail.com>
2021-11-19 12:58:38 +0200

    add helper.js

---
+++ helper.js
@@ -0,0 +1,11 @@
+// /**
+//  * @param {String} HTML representing a single element
+//  * @return {Element}
+//  */
+export function htmlToElement(html) {
+    const template = document.createElement('template');
+    html = html.trim(); // Never return a text node of whitespace as the result
+    template.innerHTML = html;
+
+    return template.content.firstChild;
+}
